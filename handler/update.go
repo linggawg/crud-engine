55660c301dcd0aaa569d869017b25d57cc259104
Evry Nazyli Ciptanto <evrynazyli@gmail.com>
2022-05-12 14:42:52 +0700

    update data with dynamic name key

--- handler/update.go
+++ handler/update.go
@@ -2,17 +2,37 @@ package handler
 
 import (
 	"crud-engine/config"
+	"encoding/json"
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 )
 
 func Update(c echo.Context) error {
 	//table := strings.ReplaceAll(c.Path(), "/", "")
+	table := c.Param("table")
+	id := c.Param("id")
 	db := config.CreateCon()
 
-	//id := c.Param("id")
-	sqlStatement := "UPDATE table_name SET column1=value, column2=value2, WHERE some_column=some_value"
+	var jsonBody map[string]interface{}
+	err := json.NewDecoder(c.Request().Body).Decode(&jsonBody)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	primaryKey, err := getPrimaryKey(db, table, c)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	var setData string
+	for key := range jsonBody {
+		setData += key + "='" + jsonBody[key].(string) + "', "
+	}
+	setData = strings.TrimRight(setData, ", ")
+
+	sqlStatement := "UPDATE " + table + " SET " + setData + " WHERE " + primaryKey + "='" + id + "'"
 
 	stmt, err := db.Prepare(sqlStatement)
 	if err != nil {
