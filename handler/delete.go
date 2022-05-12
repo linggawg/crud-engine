39007ff0bffb031b6ea50c672302a48a91139d6b
Evry Nazyli Ciptanto <evrynazyli@gmail.com>
2022-05-12 14:17:08 +0700

    delete data with dynamic name key

--- handler/delete.go
+++ handler/delete.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"crud-engine/config"
+	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
@@ -11,7 +12,12 @@ func Delete(c echo.Context) error {
 	db := config.CreateCon()
 
 	id := c.Param("id")
-	sqlStatement := "DELETE FROM " + table + " WHERE id = " + id
+	key, err := getPrimaryKey(db, table, c)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	sqlStatement := "DELETE FROM " + table + " WHERE " + key + " = " + id
 
 	stmt, err := db.Prepare(sqlStatement)
 	if err != nil {
@@ -24,4 +30,23 @@ func Delete(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusOK, result)
+}
+
+func getPrimaryKey(db *sqlx.DB, table string, c echo.Context) (key string, err error) {
+	var keyMysql KeyMySQL
+	sql := "show columns from " + table + " where `Key` = 'PRI'"
+	err = db.GetContext(c.Request().Context(), &keyMysql, sql)
+	if err != nil {
+		return "", err
+	}
+	return keyMysql.Field, nil
+}
+
+type KeyMySQL struct {
+	Field       string  `db:"Field"`
+	TypeData    string  `db:"Type"`
+	NullData    string  `db:"Null"`
+	Key         string  `db:"Key"`
+	DefaultData *string `db:"Default"`
+	Extra       *string `db:"Extra"`
 }
