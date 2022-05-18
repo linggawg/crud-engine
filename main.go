8f51f085e22f30bc165999e2a4662eb6074de437
Evry Nazyli Ciptanto <evrynazyli@gmail.com>
2022-05-18 09:04:58 +0700

    add swagger configuration in crud engine

--- main.go
+++ main.go
@@ -2,15 +2,34 @@ package main
 
 import (
 	"crud-engine/config"
+	_ "crud-engine/docs"
 	"crud-engine/handler"
 	"crud-engine/mongocontroller"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	echoSwagger "github.com/swaggo/echo-swagger"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
-	"github.com/labstack/echo/v4"
 )
 
+// @title Echo Swagger Example API
+// @version 1.0
+// @description This is a sample server server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:3000
+// @BasePath /
+// @schemes http
 func main() {
 	errEnv := godotenv.Load()
 	if errEnv != nil {
@@ -29,7 +48,17 @@ func main() {
 	}
 	log.Println("MongoDB successfully initialized")
 
+	// Echo instance
 	e := echo.New()
+
+	//// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
+
+	// Routes
+	e.GET("/", HealthCheck)
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
 
 	e.GET("/:table", handler.Get)
 	e.POST("/:table", handler.Post)
@@ -47,4 +76,18 @@ func main() {
 	log.Println("Listening to port ", os.Getenv("WEBSERVER_LISTEN_ADDRESS"))
 
 	e.Logger.Fatal(e.Start(os.Getenv("WEBSERVER_LISTEN_ADDRESS")))
+}
+
+// HealthCheck godoc
+// @Summary Show the status of server.
+// @Description get the status of server.
+// @Tags HealthCheck
+// @Accept */*
+// @Produce json
+// @Success 200 {object} map[string]interface{}
+// @Router / [get]
+func HealthCheck(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"data": "Server is up and running",
+	})
 }
