3ef30542d41395848e7873c1bc45fc458374a047
Evry Nazyli Ciptanto <evrynazyli@gmail.com>
2022-06-06 17:14:47 +0700

    unit test repository userservive and services

--- modules/models/model.go
+++ modules/models/model.go
@@ -40,18 +40,24 @@ type Dbs struct {
 	ModifiedBy *string   `db:"modified_by" json:"modified_by"`
 }
 
+// Dbs list
+type DbsList []Dbs
+
 type Services struct {
 	ID                string    `db:"id" json:"id"`
 	DbID              string    `db:"db_id" json:"db_id"`
 	Method            string    `db:"method" json:"method"`
 	ServiceUrl        string    `db:"service_url" json:"service_url"`
-	ServiceDefinition *string    `db:"service_definition" json:"service_definition"`
+	ServiceDefinition *string   `db:"service_definition" json:"service_definition"`
 	IsQuery           bool      `db:"is_query" json:"is_query"`
 	CreatedAt         null.Time `db:"created_at" json:"created_at"`
 	CreatedBy         string    `db:"created_by" json:"created_by"`
 	ModifiedAt        null.Time `db:"modified_at" json:"modified_at"`
 	ModifiedBy        string    `db:"modified_by" json:"modified_by"`
 }
+
+// Services list
+type ServicesList []Services
 
 type UserService struct {
 	ID         string    `db:"id" json:"id"`
@@ -62,3 +68,6 @@ type UserService struct {
 	ModifiedAt null.Time `db:"modified_at" json:"modified_at"`
 	ModifiedBy string    `db:"modified_by" json:"modified_by"`
 }
+
+// UserService list
+type UserServices []UserService
---
+++ modules/services/http_handler_test.go
@@ -0,0 +1,85 @@
+package services_test
+
+import (
+	"context"
+	"crud-engine/modules/models"
+	"crud-engine/modules/services"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/guregu/null.v4"
+	"testing"
+	"time"
+)
+
+var (
+	serviceDefinition = "SELECT id FROM user"
+	mockServicesList  = models.ServicesList{
+		models.Services{
+			ID:                "20bd16be-7f64-4281-9b58-758ecc7098b9",
+			DbID:              "33d2a2b4-463f-4a27-aaac-d90fbfe6ebd2",
+			Method:            "GET",
+			ServiceUrl:        "user",
+			ServiceDefinition: &serviceDefinition,
+			IsQuery:           false,
+			CreatedAt:         null.TimeFrom(time.Now()),
+			CreatedBy:         "93147c1d-197e-4556-9d24-1992196aaa03",
+			ModifiedAt:        null.TimeFrom(time.Now()),
+			ModifiedBy:        "93147c1d-197e-4556-9d24-1992196aaa03",
+		},
+		models.Services{
+			ID:                "18a53750-d3e2-4e5a-9d0c-e88d368509f7",
+			DbID:              "33d2a2b4-463f-4a27-aaac-d90fbfe6ebd2",
+			Method:            "GET",
+			ServiceUrl:        "user",
+			ServiceDefinition: &serviceDefinition,
+			IsQuery:           false,
+			CreatedAt:         null.TimeFrom(time.Now()),
+			CreatedBy:         "93147c1d-197e-4556-9d24-1992196aaa03",
+			ModifiedAt:        null.TimeFrom(time.Now()),
+			ModifiedBy:        "93147c1d-197e-4556-9d24-1992196aaa03",
+		},
+	}
+)
+
+func TestGetByServiceUrlAndMethod(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	rows := sqlmock.NewRows([]string{"id", "db_id", "method", "service_url", "service_definition", "is_query", "created_at", "created_by", "modified_at", "modified_by"})
+	for _, i := range mockServicesList {
+		rows.AddRow(i.ID, i.DbID, i.Method, i.ServiceUrl, i.ServiceDefinition, i.IsQuery, i.CreatedAt, i.CreatedBy, i.ModifiedAt, i.ModifiedBy)
+	}
+
+	query := "SELECT id, db_id, service_url, method, service_definition, is_query FROM services WHERE service_url = \\$1 AND method = \\$2"
+	mock.ExpectQuery(query).WithArgs(mockServicesList[0].ServiceUrl, mockServicesList[0].Method).WillReturnRows(rows)
+	sqlxDB := sqlx.NewDb(db, "sqlmock")
+	a := services.New(sqlxDB)
+
+	mService, err := a.GetByServiceUrlAndMethod(context.TODO(), mockServicesList[0].ServiceUrl, mockServicesList[0].Method)
+	assert.NoError(t, err)
+	assert.NotNil(t, mService)
+}
+
+func TestGetByServiceDefinitionAndMethod(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	rows := sqlmock.NewRows([]string{"id", "db_id", "method", "service_url", "service_definition", "is_query", "created_at", "created_by", "modified_at", "modified_by"})
+	for _, i := range mockServicesList {
+		rows.AddRow(i.ID, i.DbID, i.Method, i.ServiceUrl, i.ServiceDefinition, i.IsQuery, i.CreatedAt, i.CreatedBy, i.ModifiedAt, i.ModifiedBy)
+	}
+
+	query := "SELECT id, db_id, service_url, method, service_definition, is_query FROM services WHERE is_query = true AND service_definition = \\$1 AND method = \\$2"
+	mock.ExpectQuery(query).WithArgs(mockServicesList[0].ServiceDefinition, mockServicesList[0].Method).WillReturnRows(rows)
+	sqlxDB := sqlx.NewDb(db, "sqlmock")
+	a := services.New(sqlxDB)
+
+	mService, err := a.GetByServiceDefinitionAndMethod(context.TODO(), *mockServicesList[0].ServiceDefinition, mockServicesList[0].Method)
+	assert.NoError(t, err)
+	assert.NotNil(t, mService)
+}
