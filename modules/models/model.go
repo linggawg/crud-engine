0596bf3baf5db85f3269bcb66ff39fbcfc7cec8d
Evry Nazyli Ciptanto <evrynazyli@gmail.com>
2022-06-10 17:38:06 +0700

    fixing issue nil pointer after test

--- modules/models/model.go
+++ modules/models/model.go
@@ -47,7 +47,7 @@ type Services struct {
 	ID                string    `db:"id" json:"id"`
 	DbID              string    `db:"db_id" json:"db_id"`
 	Method            string    `db:"method" json:"method"`
-	ServiceUrl        string    `db:"service_url" json:"service_url"`
+	ServiceUrl        *string   `db:"service_url" json:"service_url"`
 	ServiceDefinition *string   `db:"service_definition" json:"service_definition"`
 	IsQuery           bool      `db:"is_query" json:"is_query"`
 	CreatedAt         null.Time `db:"created_at" json:"created_at"`
